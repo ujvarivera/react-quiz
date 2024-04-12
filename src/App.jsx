11cc8fa603ae2a8a67083252f32a9c352ea49a00
ujvarivera <ujvariveronika0320@gmail.com>
2024-04-12 20:26:34 +0200

    trying out trivia api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [questions, setQuestions] = useState([])
+  const URL = "https://opentdb.com/api.php?amount=10"
+  const categories = ["Any", "General Knowledge", "Entertainment: Books", "Entertainment: Film",
+    "Entertainment: Music", "Mythology", "Art", "Celebrities", "Animals"]
+
+  useEffect(() => {
+    const getQuestions = async () => {
+      const { data } = await axios.get(URL);
+      console.log(data.results)
+      setQuestions(data.results)
+    }
+    getQuestions()
+  }, [])
 
   return (
     <div>
       Quiz App
+      <div>
+          {
+            questions && questions[0]?.question
+          }
+      </div>
     </div>
   )
 }
