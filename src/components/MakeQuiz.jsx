c5dd61bd5e23d12b825c8ba5d95fb80f33fd4257
ujvarivera <ujvariveronika0320@gmail.com>
2024-04-13 11:14:59 +0200

    make quiz component created with options

--- src/components/MakeQuiz.jsx
+++ src/components/MakeQuiz.jsx
@@ -1,23 +1,64 @@
 import React, { useState } from 'react'
-import { Radio, RadioGroup, Stack } from '@chakra-ui/react'
-import { categories } from "../constants/index"
+import { Button, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Radio, RadioGroup, Stack } from '@chakra-ui/react'
+import { categories, difficulties } from "../constants/index"
 
 const MakeQuiz = () => {
+    const [questionNumber, setQuestionNumber] = useState(10)
     const [selectedCategory, setSelectedCategory] = useState("")
+    const [selectedDifficulty, setSelectedDifficulty] = useState("easy")
+    const [link, setLink] = useState("")
+
+    const startQuiz = () => {
+        if (selectedCategory === "") {
+            const apiLink = `https://opentdb.com/api.php?amount=${questionNumber}&difficulty=${selectedDifficulty}&type=multiple`
+            setLink(apiLink)
+        } else {
+            const apiLink = `https://opentdb.com/api.php?amount=${questionNumber}&category=${selectedCategory}&difficulty=${selectedDifficulty}&type=multiple`
+            setLink(apiLink)
+        }
+    }
 
     return (
         <div>
+            <p>Choose the number or questions: </p>
+            <NumberInput value={questionNumber} onChange={setQuestionNumber} min="5" max="50" maxW={20}>
+                <NumberInputField />
+                <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                </NumberInputStepper>
+            </NumberInput>
+
             <p>Choose a category:</p>
-            <RadioGroup onChange={setSelectedCategory} value={selectedCategory}>
+            <Stack direction='row'>
+                {
+                    categories.map((category) => (
+                        <Radio onChange={(e) => setSelectedCategory(e.target.value)} value={category.id} key={category.id} isChecked={selectedCategory == category.id}>{category.name}</Radio>
+                    ))
+                }
+            </Stack>
+
+            <p>Choose a difficulty:</p>
+            <RadioGroup onChange={setSelectedDifficulty} value={selectedDifficulty}>
                 <Stack direction='row'>
                     {
-                        categories.map((category) => (
-                            <Radio value={category} key={category}>{category}</Radio>
+                        difficulties.map((difficulty) => (
+                            <Radio value={difficulty} key={difficulty}>{difficulty}</Radio>
                         ))
                     }
                 </Stack>
             </RadioGroup>
-            <div>Selected: {selectedCategory}</div>
+
+            <div>
+                Selected:
+                <p>Number of Questions: {questionNumber}</p>
+                <p>Category: {selectedCategory}</p>
+                <p>Difficulty: {selectedDifficulty}</p>
+            </div>
+
+            <Button onClick={startQuiz} colorScheme='blue'>Start the game</Button>
+
+            <div>{link}</div>
         </div>
     )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,20 @@
-export const categories = ["Any", "General Knowledge", "Entertainment: Books", "Entertainment: Film",
+/*
+export const categories = "Any", "General Knowledge", "Entertainment: Books", "Entertainment: Film",
     "Entertainment: Music", "Mythology", "Art", "Celebrities", "Animals"]
+    */
+
+export const categories = [
+    { name: "Any", id: "" },
+    { name: "General Knowledge", id: 9 },
+    { name: "Entertainment: Books", id: 10 },
+    { name: "Entertainment: Film", id: 11 },
+    { name: "Entertainment: Music", id: 12 },
+    { name: "Entertainment: Video Games", id: 15 },
+    { name: "Science: Computers", id: 18 },
+    { name: "Mythology", id: 20 },
+    { name: "Art", id: 25 },
+    { name: "Celebrities", id: 26 },
+    { name: "Animals", id: 27 },
+]
+
+export const difficulties = ["easy", "medium", "hard"]
